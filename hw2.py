37513bbce7605865e810a9e70898f384a65c1389
Jessicaliu21 <wwenliu@uw.edu>
2019-11-21 18:42:27 -0800

    fix

--- hw2.py
+++ hw2.py
@@ -1,6 +1,7 @@
 ##1. Create a DataFrame from a URL that points to a CSV file. 
 
 import pandas as pd
+import numpy as np
 
 def download_to_csv(url):
     
@@ -12,17 +13,18 @@ def test_create_dataframe(df,columns):
     # condition1: there are at least 10 rows in the DataFrame
     # condition2: The values in each column have the same python data type
     # condition3: The dataframe contains only the columns that you specified as the second argument 
-    if  len(df) >= 10 and df.equals(df.dtypes) and  sorted(df.columns)==sorted(columns):
+    if  len(df) >= 10 and (np.sum([df[x].map(type).nunique() -1 for x in df.columns]) == 0) and  sorted(df.columns)==sorted(columns):
         return True
-    return False 
+    else:
+        return False 
 
 ### test case 
 
 ### Uncomment it if you need to test the module 
 
-# url='https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
-# columns=['Date', 'Fremont Bridge East Sidewalk', 'Fremont Bridge West Sidewalk']
-# df=download_to_csv(url)
-# test_create_dataframe(df,columns)
+url='https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+columns=['Date', 'Fremont Bridge Total','Fremont Bridge East Sidewalk', 'Fremont Bridge West Sidewalk']
+df=download_to_csv(url)
+print(test_create_dataframe(df,columns))
 
 
